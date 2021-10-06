87796e5ff21885924473912064c743ed46ec06d7
Kiëd Llaentenn <kiedtl@tilde.team>
2021-10-06 13:36:26 -0400

    Add keynames for alphabet

--- main.c
+++ main.c
@@ -474,7 +474,15 @@ keyname(size_t kcode)
 	char *sdl_keys[] = {
 		[SDLK_ESCAPE] = "escape",
 		[SDLK_RETURN] = "enter",
-		[SDLK_r]      = "r",
+		[SDLK_a]      = "a", [SDLK_b]      = "b", [SDLK_c]      = "c",
+		[SDLK_d]      = "d", [SDLK_e]      = "e", [SDLK_f]      = "f",
+		[SDLK_g]      = "g", [SDLK_h]      = "h", [SDLK_i]      = "i",
+		[SDLK_j]      = "j", [SDLK_k]      = "k", [SDLK_l]      = "l",
+		[SDLK_m]      = "m", [SDLK_n]      = "n", [SDLK_o]      = "o",
+		[SDLK_p]      = "p", [SDLK_q]      = "q", [SDLK_r]      = "r",
+		[SDLK_s]      = "s", [SDLK_t]      = "t", [SDLK_u]      = "u",
+		[SDLK_v]      = "v", [SDLK_w]      = "w", [SDLK_x]      = "x",
+		[SDLK_y]      = "y", [SDLK_z]      = "z"
 	};
 
 	switch (kcode) {
